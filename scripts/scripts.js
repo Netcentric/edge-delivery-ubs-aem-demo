fd2a28c84216ada29bb8655ac585b0152d855e2b
Bjoern Weide <bjoern.weide@netcentric.biz>
2025-01-31 15:08:24 +0100

    fix: remove buildHeroBlock call from buildAutoBlocks function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -32,7 +32,6 @@ async function loadFonts() {
  */
 function buildAutoBlocks(main) {
   try {
-    buildHeroBlock(main);
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Auto Blocking failed', error);
