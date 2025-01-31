106e36e7073e6f6444b1b2fb53db43b5130e77f5
Bjoern Weide <bjoern.weide@netcentric.biz>
2025-01-31 14:23:33 +0100

    fix: append next sibling to container in text-image block

--- blocks/text-image/text-image.js
+++ blocks/text-image/text-image.js
@@ -4,6 +4,13 @@ const variantClasses = ['bigimage', 'smallimage', 'v-center', 'h-center', 'image
 
 export default function decorate(block) {
   const blockName = 'text-image';
+  const container = block.querySelector(`.${blockName}__container`);
+  if (container) {
+    const nextSibling = container.nextElementSibling;
+    if (nextSibling) {
+      container.appendChild(nextSibling);
+    }
+  }
   // add variant classnames
   variantsClassesToBEM(block.classList, variantClasses, blockName);
   block.firstElementChild.classList.add(`${blockName}__container`);
