85ee5408428135acf4287e2fcc418bb849e04e38
Bjoern Weide <bjoern.weide@netcentric.biz>
2025-01-31 09:37:32 +0100

    header fix

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -144,12 +144,12 @@ export default async function decorate(block) {
   nav.id = 'nav';
   nav.classList.add('nav');
   while (fragment.firstElementChild) {
-    child = fragment.firstElementChild
+    const child = fragment.firstElementChild;
     nav.append(child);
     const h1 = child.querySelector('h1');
     if (h1) {
       while (h1.firstChild) {
-      h1.parentNode.insertBefore(h1.firstChild, h1);
+        h1.parentNode.insertBefore(h1.firstChild, h1);
       }
       h1.parentNode.removeChild(h1);
     }
