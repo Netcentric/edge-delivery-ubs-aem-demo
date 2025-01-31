013675154b37ec091a7a9784be10e0aeb0da9b2c
Bjoern Weide <bjoern.weide@netcentric.biz>
2025-01-31 14:10:55 +0100

    add 'image-right' variant to text-image block and adjust child order

--- blocks/text-image/text-image.js
+++ blocks/text-image/text-image.js
@@ -1,12 +1,15 @@
 import { variantsClassesToBEM } from '../../scripts/common.js';
 
-const variantClasses = ['bigimage', 'smallimage', 'v-center', 'h-center'];
+const variantClasses = ['bigimage', 'smallimage', 'v-center', 'h-center', 'image-right'];
 
 export default function decorate(block) {
   const blockName = 'text-image';
   // add variant classnames
   variantsClassesToBEM(block.classList, variantClasses, blockName);
-
+  if (block.classList.contains('image-right')) {
+    const secondChild = block.children[1];
+    block.insertBefore(secondChild, block.firstChild);
+  }
   block.firstElementChild.classList.add(`${blockName}__container`);
   const pictureParent = block.querySelector('picture').parentElement;
   pictureParent?.classList.add(`${blockName}__image-container`);
