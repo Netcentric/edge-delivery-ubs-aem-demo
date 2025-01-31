a8122962f1e6a9b4f7018581d3d81d1356093caf
Bjoern Weide <bjoern.weide@netcentric.biz>
2025-01-31 09:41:35 +0100

    header fix

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -144,15 +144,14 @@ export default async function decorate(block) {
   nav.id = 'nav';
   nav.classList.add('nav');
   while (fragment.firstElementChild) {
-    const child = fragment.firstElementChild;
-    nav.append(child);
-    const h1 = child.querySelector('h1');
-    if (h1) {
+    nav.append(fragment.firstElementChild);
+    const h1s = fragment.querySelectorAll('h1');
+    h1s.forEach((h1) => {
       while (h1.firstChild) {
-        h1.parentNode.insertBefore(h1.firstChild, h1);
+      h1.parentNode.insertBefore(h1.firstChild, h1);
       }
       h1.parentNode.removeChild(h1);
-    }
+    });
   }
 
   const classes = ['brand', 'sections', 'tools'];
