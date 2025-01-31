aeec9d35521c8d692373ec1be2c52b47d7998c98
Bjoern Weide <bjoern.weide@netcentric.biz>
2025-01-31 14:11:50 +0100

    fix: move variant classnames addition to the correct position in text-image block

--- blocks/text-image/text-image.js
+++ blocks/text-image/text-image.js
@@ -5,11 +5,11 @@ const variantClasses = ['bigimage', 'smallimage', 'v-center', 'h-center', 'image
 export default function decorate(block) {
   const blockName = 'text-image';
   // add variant classnames
-  variantsClassesToBEM(block.classList, variantClasses, blockName);
   if (block.classList.contains('image-right')) {
     const secondChild = block.children[1];
     block.insertBefore(secondChild, block.firstChild);
   }
+  variantsClassesToBEM(block.classList, variantClasses, blockName);
   block.firstElementChild.classList.add(`${blockName}__container`);
   const pictureParent = block.querySelector('picture').parentElement;
   pictureParent?.classList.add(`${blockName}__image-container`);
